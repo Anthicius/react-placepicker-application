de04840fa934c49ff9b8b065607fa088a8e32bd3
Anthicius <andrius.makseckas@gmail.com>
2025-07-15 16:13:30 +0300

    added Local storage for selected places to be saved in browser storage after a page reload

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,18 @@ function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
-  const [availablePlaces, setAvailablePlaces] = useState([])
+  const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       const sortedPlaces = sortPlacesByDistance(
         AVAILABLE_PLACES,
         position.coords.latitude,
         position.coords.longitude
       );
-      setAvailablePlaces(sortedPlaces)
+      setAvailablePlaces(sortedPlaces);
     });
   }, []);
-
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -42,6 +41,14 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
